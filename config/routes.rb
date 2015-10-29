82127d43d76b809833445adcb39cb33283ed441d
Exlumberjack <rwturner@wisc.edu>
2015-10-28 21:47:24 -0500

    Added json controllers for later ionic integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'user/sessions', registrations: 'user/registrations', passwords: 'user/passwords' }
   resources :visits
   resources :vets
   resources :vaccinations
