6a09bbfffbe0eee92a1889ea0bf43ba1bf3ead2c
Exlumberjack <rwturner@wisc.edu>
2015-10-31 15:56:19 -0500

    RNGESUS PLEASE

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,7 +4,7 @@ class PetsController < ApplicationController
   # GET /pets
   # GET /pets.json
   def index
-    @pets = Pet.find_by_sql("SELECT * FROM pets WHERE \"userId\" = 2")
+    @pets = Pet.find_by_sql("SELECT * FROM pets WHERE \"userId\" = ?", params[:userId])
   end
 
   # GET /pets/1
