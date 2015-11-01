be7792cfe58290254a45d15d42b1f2c7b01bdd19
Exlumberjack <rwturner@wisc.edu>
2015-11-01 11:07:29 -0600

    Updated schema to current database layout

--- db/schema.rb
+++ db/schema.rb
@@ -53,12 +53,12 @@ ActiveRecord::Schema.define(version: 20151029022619) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                             default: "", null: false
-    t.string   "encrypted_password",                default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
