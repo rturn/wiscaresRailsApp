376d0ce49b3a07c51da13525e717df40707c7f69
Adam <ahclipse@gmail.com>
2015-11-16 14:13:02 -0600

    Rspec coverage left in working condition

--- spec/pets_spec.rb
+++ spec/pets_spec.rb
@@ -1,4 +1,5 @@
 require "rails_helper"
+require "spec_helper"
 
 RSpec.describe PetsController, :type => :controller do
 	describe "GET #index" do
@@ -7,5 +8,18 @@ RSpec.describe PetsController, :type => :controller do
       		expect(response).to be_success
       		expect(response).to have_http_status(200)
     	end
+
+    	# it "assigns @pets" do
+     #  		team = Pets.create
+     #  		get :index
+     # 		expect(assigns(:pets)).to eq([pet])
+    	# end
 	end
+
+	# describe "DELETE #destroy" do
+	# 	it "deletes pet from DB successfully" do
+	# 		expect { Pet.count }.to change.by(-1)
+	# 	end
+	# end
+	
 end
