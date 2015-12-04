00b9987353a0df9a9a68252bb81d9372346c2c86
Exlumberjack <rwturner@wisc.edu>
2015-12-04 12:26:22 -0600

    Changing more image code

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -76,5 +76,6 @@ class PetsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def pet_params
       params.require(:pet).permit(:userId, :name, :species, :breed, :gender, :birthDate, :weight, :photo)
+      params.permit(:photo)
     end
 end
